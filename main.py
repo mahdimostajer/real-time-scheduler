f3728d7c710e79d8bcb20798195d820f9d6f594b
masihbr <masihbr@gmail.com>
2024-06-25 17:04:20 +0330

    Move scheduling logic to the processor class

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import math
 import random
-import copy
 
-from job import Job, PeriodicJob
+from job import Job
 from processor import Processor
 from task import Task
-
-NUMBER_OF_TASKS = 12
-NUMBER_OF_PROCESSORS = 4
-NUMBER_OF_APERIODIC_JOBS = 40
-PERIODS = [50, 40, 30, 20, 10]
-SUM_UTIL = 3
-ERROR_MARGIN = 0.1 ** 10
-
-OVERRUN_PROB = 0.2
+from utils import decide_task_criticality, get_periods
+from config import *
 
 
 def uunifast(tasks_count: int, utilization):
@@ -28,21 +20,6 @@ def uunifast(tasks_count: int, utilization):
         if all(util <= 1 for util in tasks):
             break
     return tasks
-
-
-def get_periods(n):
-    periods = []
-    for i in range(n):
-        periods.append(random.choice(PERIODS))
-    return periods
-
-
-def decision(probability):
-    return random.random() < probability
-
-
-def decide_task_criticality():
-    return decision(0.5)
 
 
 def create_tasks(task_utils, task_periods):
@@ -77,138 +54,15 @@ def allocate_processors_to_tasks(tasks: list[Task], processors: list[Processor])
             min_processor.assign_task(task)
 
 
-def create_task_jobs(task: Task, until: int) -> list[Job]:
-    jobs: list[Job] = []
-    clock = 0
-    instance_number = 1
-    while clock < until:
-
-        will_overrun = False
-        if task.high_criticality:
-            will_overrun = decision(OVERRUN_PROB)
-
-        jobs.append(
-            PeriodicJob(task=task, release_time=clock, deadline=clock + task.period, instance_number=instance_number, will_overrun=will_overrun))
-        clock += task.period
-        instance_number += 1
-    return jobs
-
-
-def create_all_jobs(tasks: list[Task], hyper_period: int) -> list[Job]:
-    tasks.sort(key=lambda task: task.period)
-
-    print(f"hyper_period={hyper_period}")
-    print_task_list(tasks)
-
-    jobs: list[Job] = []
-    for task in tasks:
-        jobs += create_task_jobs(task, hyper_period)
-    return jobs
-
-
-def pick_earliest_deadline_job(jobs: list[Job], clock: float) -> Job | None:
-    earliest_deadline_job = None
-    for job in filter(lambda job: job.release_time <= clock, jobs):
-        if earliest_deadline_job is None:
-            earliest_deadline_job = job
-        elif job.deadline < earliest_deadline_job.deadline:
-            earliest_deadline_job = job
-        else:
-            pass
-    return earliest_deadline_job
-
-
-def pick_preempt_job(jobs: list[Job], clock: float, deadline: float) -> Job | None:
-    preempt_jobs: list[Job] = list(filter(lambda job: job.release_time <= clock and job.deadline < deadline, jobs))
-    preempt_jobs.sort(key=lambda job: job.release_time)
-    return preempt_jobs[0] if len(preempt_jobs) > 0 else None
-
-
-def print_job_list(jobs: list[Job]) -> None:
-    for job in jobs:
-        print(job)
-
-
-def print_task_list(tasks: list[Task]) -> None:
-    tasks = sorted(tasks, key=lambda task: task.util, reverse=True)
-    for task in tasks:
-        print(task)
-
-
-def print_scheduled_job_list(jobs: list[Job]) -> None:
-    for job in jobs:
-        job.remaining_execution_time = job.task.execution_time
-
-    execution_intervals = []
-    for job in jobs:
-        execution_intervals += [(job, *execution_interval) for execution_interval in job.execution_intervals]
-
-    execution_intervals.sort(key=lambda x: x[1])
-    for job, interval_start, interval_finish in execution_intervals:
-        job: Job
-        execution = interval_finish - interval_start
-        print("-" * 100)
-        print(
-            f"EXECUTED\n"
-            + f"JOB={job.id} TASK={job.task.id} INSTANCE_NUMBER={job.instance_number} PERIOD={job.task.period} RELEASE={job.release_time} DEADLINE={job.deadline} EXEC_TIME={job.task.execution_time}\n"
-            + f"FROM {interval_start} TO {interval_finish} FOR {execution} SECONDS."
-        )
-        job.remaining_execution_time -= execution
-        if job.remaining_execution_time > ERROR_MARGIN:
-            print(f"TASK WAS PREEMPTED, REMAINING EXECUTION TIME IS {job.remaining_execution_time}.")
-        else:
-            print(f"EXECUTION OF TASK WAS FINISHED.")
-
-
-def edf_schedule_jobs(jobs: list[Job]) -> list[Job]:
-    scheduled_jobs: list[Job] = []
-    clock = 0
-    active_job = None
-    while jobs:
-        if clock == 0 or active_job not in jobs:
-            clock = max(clock, min([job.release_time for job in jobs]))
-            active_job = pick_earliest_deadline_job(jobs, clock)
-            active_job.start_time_list.append(clock)
-        preempt_job = pick_preempt_job(
-            jobs=jobs,
-            clock=clock + active_job.remaining_execution_time,
-            deadline=active_job.deadline,
-        )
-        if preempt_job is not None:
-            active_job.remaining_execution_time -= preempt_job.release_time - active_job.start_time_list[-1]
-            active_job.finish_time_list.append(preempt_job.release_time)
-            preempt_job.start_time_list.append(preempt_job.release_time)
-            clock = preempt_job.release_time
-            active_job = preempt_job
-        else:
-            clock += active_job.remaining_execution_time
-            active_job.finish_time_list.append(clock)
-            active_job.remaining_execution_time = 0
-            jobs.remove(active_job)
-            scheduled_jobs.append(active_job)
-
-    return scheduled_jobs
-
-
-def edf_schedule(tasks: list[Task], hyper_period: int):
-    print("\nEDF_SCHEDULE FUNCTION:")
-    jobs = create_all_jobs(tasks, hyper_period)
-    scheduled_jobs = edf_schedule_jobs(jobs)
-
-    print("\nJOBS AFTER SCHEDULING:")
-    print_scheduled_job_list(copy.deepcopy(scheduled_jobs))
-
-
 def get_aperiodic_release_times(count: int, hyper_period: int):
     release_times = []
-    for i in range(count):
+    for _ in range(count):
         release_times.append(random.randint(0, hyper_period))
-
     return release_times
 
 
 def create_aperiodic_jobs(count: int, hyper_period: int):
-    job_deadlines = get_periods(n=count)
+    job_deadlines = get_periods(n=count, periods_list=PERIODS)
     job_release_times = get_aperiodic_release_times(count=count, hyper_period=hyper_period)
 
     jobs = []
@@ -223,7 +77,7 @@ def create_aperiodic_jobs(count: int, hyper_period: int):
 def schedule():
     try:
         task_utils = uunifast(tasks_count=NUMBER_OF_TASKS, utilization=SUM_UTIL)
-        task_periods = get_periods(n=NUMBER_OF_TASKS)
+        task_periods = get_periods(n=NUMBER_OF_TASKS, periods_list=PERIODS)
 
         tasks = create_tasks(task_utils=task_utils, task_periods=task_periods)
         hyper_period = math.lcm(*[task.period for task in tasks])
@@ -237,7 +91,7 @@ def schedule():
             processor.calculate_server_utilization()
             print("\nPROCESSOR:")
             print(processor)
-            edf_schedule(processor.tasks, hyper_period)
+            processor.edf_schedule(until=hyper_period)
     except Exception as e:
         print(e)
 
--- processor.py
+++ processor.py
@@ -1,6 +1,10 @@
 from itertools import count
 
 from task import Task
+from job import Job, PeriodicJob
+from utils import print_task_list, print_scheduled_job_list, decision
+import copy
+from config import *
 
 
 class Processor:
@@ -9,6 +13,7 @@ class Processor:
     def __init__(self):
         self.id: int = next(self.id_counter)
         self.tasks: list[Task] = []
+        self.jobs: list[Job] = []
         self.util: float = 0
         self.server_utilization = None
 
@@ -39,3 +44,92 @@ class Processor:
 
     def __str__(self) -> str:
         return f"PROC=> id={self.id}: util={self.util} tasks={[str(task) for task in self.tasks]}"
+
+    def create_all_jobs(self, until: int) -> list[Job]:
+        self.tasks.sort(key=lambda task: task.period)
+
+        print(f"Going to create Jobs from below tasks until {until}.")
+        print_task_list(self.tasks)
+
+        jobs: list[Job] = []
+        for task in self.tasks:
+            jobs += self.create_task_jobs(task, until)
+        return jobs
+
+    @staticmethod
+    def create_task_jobs(task: Task, until: int) -> list[Job]:
+        jobs: list[Job] = []
+        clock = 0
+        instance_number = 1
+        while clock < until:
+
+            will_overrun = False
+            if task.high_criticality:
+                will_overrun = decision(OVERRUN_PROB)
+
+            jobs.append(
+                PeriodicJob(
+                    task=task,
+                    release_time=clock,
+                    deadline=clock + task.period,
+                    instance_number=instance_number,
+                    will_overrun=will_overrun,
+                )
+            )
+            clock += task.period
+            instance_number += 1
+        return jobs
+
+    def pick_earliest_deadline_job(self, clock: float) -> Job | None:
+        earliest_deadline_job = None
+        for job in filter(lambda job: job.release_time <= clock, self.jobs):
+            if earliest_deadline_job is None:
+                earliest_deadline_job = job
+            elif job.deadline < earliest_deadline_job.deadline:
+                earliest_deadline_job = job
+            else:
+                pass
+        return earliest_deadline_job
+
+    def pick_preempt_job(self, clock: float, deadline: float) -> Job | None:
+        preempt_jobs: list[Job] = list(
+            filter(lambda job: job.release_time <= clock and job.deadline < deadline, self.jobs)
+        )
+        preempt_jobs.sort(key=lambda job: job.release_time)
+        return preempt_jobs[0] if len(preempt_jobs) > 0 else None
+
+    def edf_schedule_jobs(self) -> list[Job]:
+        scheduled_jobs: list[Job] = []
+        clock = 0
+        active_job = None
+        while self.jobs:
+            if clock == 0 or active_job not in self.jobs:
+                clock = max(clock, min([job.release_time for job in self.jobs]))
+                active_job = self.pick_earliest_deadline_job(clock)
+                active_job.start_time_list.append(clock)
+            preempt_job = self.pick_preempt_job(
+                clock=clock + active_job.remaining_execution_time,
+                deadline=active_job.deadline,
+            )
+            if preempt_job is not None:
+                preempt_job: Job
+                active_job.remaining_execution_time -= preempt_job.release_time - active_job.start_time_list[-1]
+                active_job.finish_time_list.append(preempt_job.release_time)
+                preempt_job.start_time_list.append(preempt_job.release_time)
+                clock = preempt_job.release_time
+                active_job = preempt_job
+            else:
+                clock += active_job.remaining_execution_time
+                active_job.finish_time_list.append(clock)
+                active_job.remaining_execution_time = 0
+                self.jobs.remove(active_job)
+                scheduled_jobs.append(active_job)
+
+        return scheduled_jobs
+
+    def edf_schedule(self, until: int):
+        print("\nEDF_SCHEDULE FUNCTION:")
+        self.jobs = self.create_all_jobs(until)
+        scheduled_jobs = self.edf_schedule_jobs()
+        print("\nJOBS AFTER SCHEDULING:")
+        print_scheduled_job_list(copy.deepcopy(scheduled_jobs))
